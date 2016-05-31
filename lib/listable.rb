e060e87f7e1d04ca43c2250c35eb85311d9a23c7
Masruba Tasnim <masruba@gmail.com>
2016-05-31 15:46:35 -0700

    Moved Fixed date to listable.rb

--- lib/listable.rb
+++ lib/listable.rb
@@ -8,13 +8,22 @@ module Listable
   	if options.key?(:start_date)
 	  	start_data = options[:start_date]
 	  	end_date = options[:end_date]
-	    dates = @start_date.strftime("%D") if @start_date
-	    dates << " -- " + @end_date.strftime("%D") if @end_date
+	    dates = @start_date.strftime("%D") if start_date
+	    dates << " -- " + @end_date.strftime("%D") if end_date
 	    dates = "N/A" if !dates
 	    return dates
 	  else
 	  	due =  options[:due]
-	    @due ? @due.strftime("%D") : "No due date"
+	    due ? due.strftime("%D") : "No due date"
 	  end
   end  
+
+  def format_priority(value)
+    value = " ⇧" if @priority == "high"
+    value = " ⇨" if @priority == "medium"
+    value = " ⇩" if @priority == "low"
+    value = "" if !@priority
+    return value
+  end
+
 end
